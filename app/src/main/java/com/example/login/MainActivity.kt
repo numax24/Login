1dfe541848015084cc3b35372aa359983f8b5b53
numax24 <imanuelspl4@gmail.com>
2024-03-13 15:55:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textRegister.setOnClickListener(View.OnClickListener {startActivity(Intent(this, Register::class.java)) })
+        textForgot.setOnClickListener(View.OnClickListener {startActivity(Intent(this, Forget::class.java)) })
+        btnLogin.setOnClickListener(View.OnClickListener {startActivity(Intent(this, Home::class.java)) })
+    }
+}
